008ee8bdb6ee74756db0339f53d0cab81cbad549
iHackFX <32912056+iHackFX@users.noreply.github.com>
2021-03-29 03:09:00 +0300

    v0.1

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,23 +1,75 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonFab,
+  IonFabButton,
+  IonIcon,
+  IonFabList,
+} from "@ionic/react";
+import React, { useEffect, useState } from "react";
+import { add, trendingDown, trendingUp, wallet } from "ionicons/icons";
+import ExploreContainer from "../components/ExploreContainer";
+import "./Tab1.css";
+import { AddDataModal } from "../components/addDataModal";
+import { KeyboardStyle, Plugins } from "@capacitor/core";
+
+const { Storage } = Plugins;
 
 const Tab1: React.FC = () => {
+  const [showState, setShowState] = useState(false);
+  const [data, setData] = useState("");
+  // const [parsedData, setParsedData] = useState<Object>();
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>
+            <IonIcon icon={wallet} /> Мои деньги
+          </IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
+            <IonTitle size="large"></IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <AddDataModal
+          setShowState={setShowState}
+          showState={showState}
+          typeOfData={data}
+        />
+        {1 > 0
+        ? "" 
+        : <ExploreContainer name="Денег пока нет, добавьте их чтобы увидеть информацию о доходах и расходах" />
+        }
       </IonContent>
+      <IonFab vertical="bottom" horizontal="end" slot="fixed">
+        <IonFabButton>
+          <IonIcon icon={add} />
+        </IonFabButton>
+        <IonFabList side="top">
+          <IonFabButton
+            onClick={() => {
+              setData("Расход");
+              setShowState(true);
+            }}
+          >
+            <IonIcon icon={trendingDown} />
+          </IonFabButton>
+          <IonFabButton
+            onClick={() => {
+              setData("Доход");
+              setShowState(true);
+            }}
+          >
+            <IonIcon icon={trendingUp} />
+          </IonFabButton>
+        </IonFabList>
+      </IonFab>
     </IonPage>
   );
 };
--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,4 +1,5 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonIcon, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { analytics } from 'ionicons/icons';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab3.css';
 
@@ -7,16 +8,15 @@ const Tab3: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
+          <IonTitle><IonIcon icon={analytics} /> Статистика</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
+            <IonTitle size="large"></IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
       </IonContent>
     </IonPage>
   );
